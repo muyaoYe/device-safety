8a3e78ee2ed6d26a0d58d8db57e07dba695c3728
18359533584 <463973947@qq.com>
2021-05-22 15:36:57 +0800

    搭建项目

---
+++ src/main/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplication.java
@@ -0,0 +1,13 @@
+package cn.soc.securityoperationscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOperationsCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOperationsCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.soc.securityoperationscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOperationsCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
