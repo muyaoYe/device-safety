399b3cc66b4173bcfba4e834fcdbd74b2a392459
xjj <2433268029@qq.com>
2021-06-29 23:52:38 +0800

    1

--- src/main/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplication.java
+++ src/main/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplication.java
@@ -2,14 +2,16 @@ package cn.soc.securityoperationscenter;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-@MapperScan("cn.soc.securityoperationscenter.dao")
+@EnableAutoConfiguration
+
+//@ComponentScan(basePackages = { "cn.soc.securityoperationscenter" })
+@MapperScan(value = "cn.soc.securityoperationscenter.dao")
 
 public class SecurityOperationsCenterApplication {
 
