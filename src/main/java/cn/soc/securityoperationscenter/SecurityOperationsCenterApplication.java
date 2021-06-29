2390c4990de14e962f097ff75aba7007fa4fb0c8
ciel <604019297@qq.com>
2021-06-30 01:07:24 +0800

    修改了启动类SecurityOperationsCenterApplication的@EnableTransactionManagement注释

--- src/main/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplication.java
+++ src/main/java/cn/soc/securityoperationscenter/SecurityOperationsCenterApplication.java
@@ -2,16 +2,12 @@ package cn.soc.securityoperationscenter;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-
-//@ComponentScan(basePackages = { "cn.soc.securityoperationscenter" })
-@MapperScan(value = "cn.soc.securityoperationscenter.dao")
+@EnableTransactionManagement
+@MapperScan("cn.soc.securityoperationscenter.dao")
 
 public class SecurityOperationsCenterApplication {
 
